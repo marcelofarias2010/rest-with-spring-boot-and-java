9d4721ebe630152ef114a8ce0b6d2b1169087da6
marcelofarias2010 <mljinformatica@gmail.com>
2025-07-03 15:27:41 -0300

    Dockerizing our RESTfull API Application

--- src/test/java/br/com/farias/rest_with_spring_boot_and_java/integrationtests/swagger/SwaggerIntegrationTest.java
+++ src/test/java/br/com/farias/rest_with_spring_boot_and_java/integrationtests/swagger/SwaggerIntegrationTest.java
@@ -8,14 +8,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static io.restassured.RestAssured.given;
 import static junit.framework.TestCase.assertTrue;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class SwaggerIntegrationTest extends AbstractIntegrationTest {
 
     @Test
     void shouldDisplaySwaggerUIPage() {
         var content = given()
                 .basePath("/swagger-ui/index.html")
-                    .port(TestConfigs.SERVER_PORT)
+                    //.port(TestConfigs.SERVER_PORT)
                 .when()
                     .get()
                 .then()
